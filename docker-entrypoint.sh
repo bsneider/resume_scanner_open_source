1e9346ee3619e423f14d34f85c8330802c505f14
bsneider <54788551+bsneider@users.noreply.github.com>
2021-11-19 15:26:43 -0600

    handle server migrations

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Create the migrations
+echo "Create the migrations"
+python manage.py makemigrations
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
--- resume_scanner/settings.py
+++ resume_scanner/settings.py
@@ -67,12 +67,12 @@ WSGI_APPLICATION = 'resume_scanner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }
 
 
 # Internationalization
